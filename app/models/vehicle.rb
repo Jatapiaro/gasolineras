e3abc6a3a37089572494933f98de588bae24f788
Jacobo <jacob.et.cetera@gmail.com>
2017-03-10 00:41:34 -0600

    for heroku

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,9 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :brand, presence: true, length: {minimum: 3}
+  validates :model, presence: true, length: {minimum: 3}
+  validates :year, presence: true, length: {minimum: 4}
+
 end
