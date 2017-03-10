e40aaa0c60d76b8fc8a0f141aae10002cbf94b0c
Jacobo <jacob.et.cetera@gmail.com>
2017-03-09 23:57:01 -0600

    Funcionando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,9 @@
 class ApplicationController < ActionController::Base
   	 
-    before_action :authenticate_user!##, :do_not_set_cookie, if: -> { request.format.json? }
+    before_action :authenticate_user!, :do_not_set_cookie, if: -> { request.format.json? }
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protect_from_forgery unless: -> { request.format.json? }
-
-    env['rack.session.options'][:skip] = true unless -> { request.format.json? }
-
-
-    #Require authentication and do not set a session cookie for JSON requests (API clients)
-    ##before_action :authenticate_user!, :do_not_set_cookie, if: -> { request.format.json? }
-    ##protect_from_forgery unless: -> { request.format.json? }
 
     protected
     def configure_permitted_parameters
@@ -22,7 +15,7 @@ class ApplicationController < ActionController::Base
     # Do not generate a session or session ID cookie
     # See https://github.com/rack/rack/blob/master/lib/rack/session/abstract/id.rb#L171
     def do_not_set_cookie
-      ##puts "Estoy eliminando o saltandome las cookies"
-      env['rack.session.options'][:skip] = true
+      puts "Estoy eliminando o saltandome las cookies"
+      reset_session
     end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,28 +1,23 @@
-class Users::SessionsController < Devise::SessionsController
+module Api
+  module V1
+    class Users::SessionsController < Devise::SessionsController
+      skip_before_action :verify_signed_out_user
 
-  
-  skip_before_action :verify_signed_out_user
-  
-  def create
-    user = warden.authenticate!(auth_options)
-    token = Tiddle.create_and_return_token(user, request)
-    render json: { authentication_token: token,
-    user: user}
-  end
+      def create
+        user = warden.authenticate!(:scope => :user)
+        token = Tiddle.create_and_return_token(user, request)
+        render json: { authentication_token: token, user: user }
+      end
 
-  def destroy
-    if current_user && Tiddle.expire_token(current_user, request)
-      head :ok
-    else
-      # Client tried to expire an invalid token
-      render json: { error: 'invalid token' }, status: 401
+      def destroy
+        if current_user && Tiddle.expire_token(current_user, request)
+          head :ok
+        else
+          # Client tried to expire an invalid token
+          render json: { error: 'invalid token' }, status: 401
+        end
+      end
+
     end
   end
-
-
-  private
-
-    # this is invoked before destroy and we have to override it
-    def verify_signed_out_user
-    end
 end
