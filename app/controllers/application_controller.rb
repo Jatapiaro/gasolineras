2e26dae36a7c76bc57f4fc67b3004097c99bafca
Jacobo <jacob.et.cetera@gmail.com>
2017-04-05 02:57:07 -0500

    Conetcatdo2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,21 +19,5 @@ class ApplicationController < ActionController::Base
       puts "Estoy eliminando o saltandome las cookies"
       reset_session
     end
-
-      def location
-        if params[:location].blank?
-          if Rails.env.test? || Rails.env.development?
-            @location ||= Geocoder.search("50.78.167.161").first
-          else
-            @location ||= request.location
-          end
-        else
-          params[:location].each {|l| l = l.to_i } if params[:location].is_a? Array
-          @location ||= Geocoder.search(params[:location]).first
-          allowed = [:latitude, :longitude, :address, :city]
-          puts @location.methods
-          @location
-        end
-      end
-
+    
 end
