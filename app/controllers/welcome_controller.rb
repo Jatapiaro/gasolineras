23d238a907b5d0fce79310bf70a0b66fffdfa92a
Jacobo <jacob.et.cetera@gmail.com>
2017-04-05 23:32:16 -0500

    Brute addition of gmaps

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,28 +3,34 @@ class WelcomeController < ApplicationController
 
 
 	def index
-		##mOe7CawCpvO1Xn/19.2834775/-99.1352799
 		url = "https://gasolineria.herokuapp.com/developer/estacionesSinCosto"
 		key = "ViTThTQd9jLCSvFGZxNuDpRwFhhabalL"
 
 		@lat
 		@long
-		##Hardcoding my ip
-		if Rails.env.development?
-			@lat = '19.2564776'
-			@long = '-99.0184185'
-	  	else
-			@lat = request.location.latitude.to_s
-	  		@long = request.location.longitude.to_s
-	  	end
+		if user_signed_in?
+			##Hardcoding my ip
+			if Rails.env.development?
+				@lat = '19.2564776'
+				@long = '-99.0184185'
+		  	else
+				@lat = request.location.latitude.to_s
+		  		@long = request.location.longitude.to_s
+		  	end
 
-	  	stations = Array.new
+		  	@stations = Array.new
 
-	  	var = HTTParty.get(url+"/"+key+"/"+@lat+"/"+@long)
-	  	data = var.parsed_response["gasolineras"]
-	  	data.each { |x| stations.push(Station.new(x))}
-	  	##
-	  	Rails.logger.debug("Usuario: "+stations[0].to_s)
+		  	var = HTTParty.get(url+"/"+key+"/"+@lat+"/"+@long)
+		  	data = var.parsed_response["gasolineras"]
+		  	data.each { |x| @stations.push(Station.new(x))}
+
+		  	@hash = Gmaps4rails.build_markers(@stations) do |s, marker|
+	 			marker.lat s.latitud
+	  			marker.lng s.longitud
+	  			marker.title s.nombre
+			end			
+		end
+	  	##Rails.logger.debug("Usuario: "+stations[0].to_s)
 	end
 
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -3,10 +3,14 @@ class Station
 
 	define_attribute_methods :direccion, :distancia, :nombre, :latitud, :longitud, :id
 
-	def initialize(h)
+	  def initialize(h)
     	h.each {|k,v| public_send("#{k}=",v)}
   	end
   	
+    def direccion
+      @direccion
+    end
+
   	def direccion=(value)
   	  @direccion = value
   	end
@@ -15,20 +19,40 @@ class Station
   	  @distancia = value
   	end
 
+    def distancia
+      @distancia
+    end
+
   	def nombre=(value)
   	  @nombre = value
   	end
+
+    def nombre
+      @nombre
+    end
 
   	def latitud=(value)
   	  @latitud = value
   	end
 
+    def latitud
+      @latitud
+    end
+
   	def longitud=(value)
   	  @longitud = value
   	end
 
+    def longitud
+      @longitud
+    end
+
   	def id=(value)
   	  @id = value
   	end
+
+    def id
+      @id
+    end
   	
 end
