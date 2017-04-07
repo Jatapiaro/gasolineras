405808145b85d9473555d1f9c03291d1f6ecfe8c
Jacobo <jacob.et.cetera@gmail.com>
2017-04-07 00:10:57 -0500

    Refactor code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,15 @@
-module Api
-  module V1
-    class Users::SessionsController < Devise::SessionsController
-      skip_before_action :verify_signed_out_user
+class Users::SessionsController < Devise::SessionsController
+  skip_before_action :verify_signed_out_user
 
-      def create
-        user = warden.authenticate!(:scope => :user)
-        token = Tiddle.create_and_return_token(user, request)
-        render json: { authentication_token: token, user: user }
-      end
-
-      def destroy
-        Tiddle.expire_token(current_user, request) if current_user
-        render json: {loged_out: "loged_out"}
-      end
-
-    end
+  def create
+    user = warden.authenticate!(:scope => :user)
+    token = Tiddle.create_and_return_token(user, request)
+    render json: { authentication_token: token, user: user }
   end
+
+  def destroy
+    Tiddle.expire_token(current_user, request) if current_user
+    render json: {loged_out: "loged_out"}
+  end
+
 end
